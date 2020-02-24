4049ecf6035c1b42d851768cdbd88f3782189659
lutianzhou001 <18015565550@189.cn>
2020-02-24 22:25:44 +0800

    add check in omni network

--- src/omni/omni.service.ts
+++ src/omni/omni.service.ts
@@ -2,10 +2,9 @@ import { Injectable, Post, Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository, UpdateDateColumn } from 'typeorm';
 import { Omni, Account, Transaction } from '../database/database.entity';
+import { omniCheck, omnigetCurrentBlock, omnigetTransaction } from './utils';
 //import { hotWallet, coins } from './config'
 const request = require('request');
-// here we can't import web3 into Ts. For more you can refer to "https://github.com/ethereum/web3.js/issues/1597"
-
 
 @Injectable()
 export class OmniService {
@@ -13,7 +12,7 @@ export class OmniService {
         @InjectRepository(Account)
         private readonly accountRepository: Repository<Account>,
         @InjectRepository(Omni)
-        private readonly blockchainRepository: Repository<Omni>,
+        private readonly omniRepository: Repository<Omni>,
         @InjectRepository(Transaction)
         private readonly transactionRepository: Repository<Transaction>,
     ) { }
@@ -46,11 +45,70 @@ export class OmniService {
     async collect() {
     }
 
+    async engine(value, id, coin_name): Promise<Boolean> {
+        // TODO
+        // send to engine
+        return true
+    }
+
     // 函数作用，从INFURA.IO 上再检查一遍 交易是否存在了
     async check(hash, from, to, value, input) {
     }
-   
-    async fetch(){
-    }
 
+
+
+    async fetch() {
+        let db: number;
+        const ocb = await omnigetCurrentBlock()
+        const maxBlock = await this.omniRepository.find({
+            order: {
+                block: 'DESC',
+            },
+            take: 1,
+        });
+        if (maxBlock.length === 0) {
+            db = 1000;
+        } else {
+            db = maxBlock[0].block;
+        }
+
+        if (db < ocb.block - 10) {
+            for (let i = db + 1; i < ocb.block - 10; i++) {
+                const block = await omniCheck(i);
+                console.log("checking" + i + "block")
+                // tslint:disable-next-line: prefer-for-of
+                for (let j = 0; j < block.length; j++) {
+                    const transaction = await omnigetTransaction(block[j])
+                    const users = await this.findUsers();
+                    for (let i = 0; i < users.length; i++) {
+                        // 这里还需要看下合约地址是否是配置中的ERC20如果不是，则也可以不入库
+                        if ((users[i].btcAddress === transaction.result.referenceaddress) && (transaction.result.propertyid == 31)) {
+                            const newTransaction = new Omni();
+                            newTransaction.block = transaction.result.block;
+                            newTransaction.amount = transaction.result.amount;
+                            newTransaction.txid = transaction.result.txid;
+                            newTransaction.sendingAddress = transaction.result.sendingaddress;
+                            newTransaction.referenceAddress = transaction.result.referenceaddress;
+                            newTransaction.isCollected = 0;
+                            newTransaction.isUpdated = 0;
+                            const result = await this.omniRepository.save(newTransaction);
+                            if (result) {
+                                //const checkResult = await this.check(transaction.hash, transaction.from, transaction.to, transaction.value, transaction.input);
+                                const engineResult = await this.engine(transaction.result.amount, users[i].id, "OMNIUSDT")
+                                if (engineResult) {
+                                    const update = await this.omniRepository.update(
+                                        result,
+                                        { isUpdated: 1 },
+                                    )
+                                    if (update) {
+                                        Logger.log("update success");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
--- src/omni/utils.js
+++ src/omni/utils.js
@@ -25,14 +25,39 @@ async function omniCheck(blockNumber){
             }
         });
     });
+    let resultCheck = await promiseCheck.then(function (value) { return value });
+    return resultCheck
+}
 
-    resultCheck = await promiseCheck.then(function (value) { return value });
-    console.log(resultCheck);
+async function omnigetCurrentBlock(){
+
+    var dataString = '{"jsonrpc": "1.0", "id":"curltest", "method": "omni_getcurrentconsensushash", "params": [] }';
+
+    var options = {
+        url: 'http://127.0.0.1:8332/',
+        method: 'POST',
+        headers: headers,
+        body: dataString,
+        auth: {
+            'user': 'vincent',
+            'pass': 'qaz123-QAZ123'
+        }
+    };
+
+    var promiseCheck = new Promise(function (resolve, reject) {
+        request(options, function (error, response, body) {
+            console.log(response)
+            if (!error && response.statusCode == 200) {
+                resolve (body);
+            }
+        });
+    });
+    let resultCheck = await promiseCheck.then(function (value) { return value });
+    return resultCheck
 }
 
 async function omnigetTransaction(transactionHash){
     var dataString = '{"jsonrpc": "1.0", "id":"curltest", "method": "omni_gettransaction", "params": ["'+ transactionHash +'"] }';
-    console.log(dataString);
     var options = {
         url: 'http://47.75.138.67:8332/',
         method: 'POST',
@@ -52,10 +77,8 @@ async function omnigetTransaction(transactionHash){
             }
         });
     });
-
-    resultCheck = await promiseCheck.then(function (value) { return value });
-    console.log(resultCheck);
+    let resultCheck = await promiseCheck.then(function (value) { return value });
+    return resultCheck;
 }
 
-omniCheck(100);
-omnigetTransaction("1fbf69015263620052fd30cd7d46c75efc8647a1f56d3c52769d6b1497a38e1f");
+export {omniCheck,omnigetCurrentBlock,omnigetTransaction}
